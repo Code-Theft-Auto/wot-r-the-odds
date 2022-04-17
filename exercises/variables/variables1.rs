4de329a29cd043542c794c12eea76742edde1c53
DevER-M <sankam.mithunganeshviib@gmail.com>
2022-04-17 18:00:44 +0530

    i mined off camera

---
+++ exercises/variables/variables1.rs
@@ -0,0 +1,10 @@
+// variables1.rs
+// Make me compile!
+// Execute the command `rustlings hint variables1` if you want a hint :)
+
+// I AM NOT DONE
+
+fn main() {
+    x = 5;
+    println!("x has the value {}", x);
+}
